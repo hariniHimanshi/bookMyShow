ef1d172bfa66397fcd134304fc2409c6dabfacc8
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-12-25 12:38:51 +0530

    init

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/screens/Login';
+import Home from './src/screens/Home';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Home: undefined; // Add parameters if needed
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: true }} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
