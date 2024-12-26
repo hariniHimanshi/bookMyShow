469e6793c61faca3ca1ef2a46d5cef179c8101ab
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-12-26 15:57:46 +0530

    add registration page

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Home from './src/screens/Home';
+import RegisterScreen from './src/screens/Registration';
 
 export type RootStackParamList = {
   Login: undefined;
-  Home: undefined; // Add parameters if needed
+  Home: { username: string };
+  Register: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -16,6 +18,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: true }} />  
         <Stack.Screen name="Home" component={Home} options={{ headerShown: true }} />
       </Stack.Navigator>
       <StatusBar style="auto" />
